10ea5a4ca6fc175edf158033d26a68bd6dda2ba0
Grzegorz Slowikowski <gslowikowski@gmail.com>
2013-04-14 07:35:05 +0000

    Initial commit: plugin, default-parents, test projects.
    git-svn-id: http://play2-maven-plugin.googlecode.com/svn/trunk/test-projects@6 0a1f0b36-0e7f-8a7b-3831-50ad4cbb12b1

---
+++ scala/zentasks/test/ApplicationSpec.scala
@@ -0,0 +1,28 @@
+package test
+
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class ApplicationSpec extends SpecificationWithJUnit {
+  
+  
+  "Application" should {
+    
+    
+    "go to login page without credentials" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val result  = route( FakeRequest( GET, "/")).get
+        status(result) must equalTo(303)
+      }      
+    }
+    "list the secured product page with credentials" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val result  = route( FakeRequest( GET, "/").withSession("email"->"guillaume@sample.com")).get
+        status(result) must equalTo(200)
+      }      
+    }
+    
+  }
+}
