c008f02fc8fbaf5790c7d6f7c243e4c1eef33103
Grzegorz Slowikowski <gslowikowski@gmail.com>
2013-10-22 19:03:50 +0000

    play2-maven-plugin/play2-maven-plugin#14 - mavenize Play! SecureSocial Module v.2.1.1 - test projects.
    git-svn-id: http://play2-maven-plugin.googlecode.com/svn/trunk/test-projects@243 0a1f0b36-0e7f-8a7b-3831-50ad4cbb12b1

---
+++ play21/external-modules/securesocial/scala/app/service/MyEventListener.scala
@@ -0,0 +1,28 @@
+package service
+
+import securesocial.core._
+import play.api.mvc.{Session, RequestHeader}
+import play.api.{Application, Logger}
+
+/**
+ * A sample event listener
+ */
+class MyEventListener(app: Application) extends EventListener {
+  override def id: String = "my_event_listener"
+
+  def onEvent(event: Event, request: RequestHeader, session: Session): Option[Session] = {
+    val eventName = event match {
+      case e: LoginEvent => "login"
+      case e: LogoutEvent => "logout"
+      case e: SignUpEvent => "signup"
+      case e: PasswordResetEvent => "password reset"
+      case e: PasswordChangeEvent => "password change"
+    }
+
+    Logger.info("traced %s event for user %s".format(eventName, event.user.fullName))
+    // Not changing the session so just return None
+    // if you wanted to change the session then you'd do something like
+    // Some(session + ("your_key" -> "your_value"))
+    None
+  }
+}
