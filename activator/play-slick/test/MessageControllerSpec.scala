db01149abd6b43e83be1b0fb00e37eae5020932e
Grzegorz Slowikowski <gslowikowski@gmail.com>
2015-05-19 12:11:21 +0000

    Test projects - activator/play-slick - fixed tests.
    git-svn-id: http://play2-maven-plugin.googlecode.com/svn/trunk/test-projects@1266 0a1f0b36-0e7f-8a7b-3831-50ad4cbb12b1

--- activator/play-slick/test/MessageControllerSpec.scala
+++ activator/play-slick/test/MessageControllerSpec.scala
@@ -1,6 +1,8 @@
 package test
 
 import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
@@ -9,6 +11,7 @@ import play.api.http.ContentTypes.JSON
 /**
  * Specs2 tests
  */
+@RunWith(classOf[JUnitRunner])
 class MessageControllerSpec extends Specification {
   
   "MessageController" should {
