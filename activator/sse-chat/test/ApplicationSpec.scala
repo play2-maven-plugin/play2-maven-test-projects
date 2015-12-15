e72cfc6da50ddd8fab4e7e942e58d5346f308bc8
Grzegorz Slowikowski <gslowikowski@gmail.com>
2015-12-15 17:22:57 +0100

    Strange. Adding:
    <systemPropertyVariables>
        <specs2.sequential>true</specs2.sequential> <!-- see https://github.com/playframework/playframework/issues/4253 -->
    </systemPropertyVariables>
    
    to Surefire plugin configuration works, but adding:
    
      /*
       * Running tests in parallel (which would ordinarily be the default) will work only if no
       * shared resources are used (e.g. top-level actors with the same name or the
       * system.eventStream).
       *
       * It's usually safer to run the tests sequentially.
       */
      sequential
    
    to specs classes does not work.

--- activator/sse-chat/test/ApplicationSpec.scala
+++ activator/sse-chat/test/ApplicationSpec.scala
@@ -13,15 +13,6 @@ import play.api.test.Helpers._
 @org.junit.runner.RunWith(classOf[org.specs2.runner.JUnitRunner])
 class ApplicationSpec extends Specification {
   
-  /*
-   * Running tests in parallel (which would ordinarily be the default) will work only if no
-   * shared resources are used (e.g. top-level actors with the same name or the
-   * system.eventStream).
-   *
-   * It's usually safer to run the tests sequentially.
-   */
-  sequential
-
   "Application" should {
     
     "send 404 on a bad request" in {
