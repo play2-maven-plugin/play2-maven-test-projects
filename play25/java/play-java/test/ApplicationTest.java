679e664f99ad92a3699142db88e50db58a10a3dd
Grzegorz Slowikowski <gslowikowski@gmail.com>
2017-03-02 16:47:18 +0100

    Changes in project play25/java/play-java reflecting latest changes in https://github.com/playframework/play-java/tree/master.

--- play25/java/play-java/test/ApplicationTest.java
+++ play25/java/play-java/test/ApplicationTest.java
@@ -21,11 +21,11 @@ import static org.junit.Assert.*;
 
 
 /**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+ *
+ * Simple (JUnit) tests that can call all parts of a play app.
+ * If you are interested in mocking a whole application, see the wiki for more details.
+ *
+ */
 public class ApplicationTest {
 
     @Test
